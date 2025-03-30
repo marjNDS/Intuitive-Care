ff0c72c80e4962fd24f6a73ff1507d58def192cd
marjNDS <135056687+marjNDS@users.noreply.github.com>
2025-03-29 21:56:15 -0300

    good practices fix

--- 1. webScraping/main.py
+++ 1. webScraping/main.py
@@ -3,13 +3,14 @@ from zipper import zip_files
 from scraper import get_file_links_by_keyword_and_extension
 from pathlib import Path
 
-site_url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
-filters = {
-    "Anexo I": ".pdf",
-    "Anexo II": ".pdf",
-}
+if __name__ == "__main__":
+    site_url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+    filters = {
+        "Anexo I": ".pdf",
+        "Anexo II": ".pdf",
+    }
 
-pdf_urls = get_file_links_by_keyword_and_extension(site_url, filters)
-files_paths = download_files(pdf_urls)
-print(files_paths)
-zip_files(files = files_paths, output_zip_path="downloads/compactado.zip")
+    pdf_urls = get_file_links_by_keyword_and_extension(site_url, filters)
+    files_paths = download_files(pdf_urls)
+    print(files_paths)
+    zip_files(files=files_paths, output_zip_path="downloads/compactado.zip")
